6d430f36970f46383c048d2497befcd46780e883
Lammatian <mateusz.parafinski@gmail.com>
2021-12-04 22:02:36 +0100

    Add day 02

--- 2021/day2/day2.java
+++ 2021/day2/day2.java
@@ -1,70 +1,85 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class day2 {
-    public static int part1() {
-        int result = 0;
-        int forward = 0;
-        int vertical = 0;
-        BufferedReader in = null;
+    static class Instruction {
+        String command;
+        int value;
+
+        Instruction(String c, int v) {
+            command = c;
+            value = v;
+        }
+    }
+
+    public static ArrayList<Instruction> readInput(String filename) {
+        BufferedReader in;
+        ArrayList<Instruction> instructions = new ArrayList<>();
         try {
-            in = new BufferedReader(new FileReader("2021/data.txt"));
+            in = new BufferedReader(new FileReader(filename));
             String l;
             while ((l = in.readLine()) != null) {
                 String[] parts = l.split(" ");
-                switch (parts[0]) {
-                    case "forward":
-                        forward = forward + Integer.parseInt(parts[1]);
-                        break;
-                    case "down":
-                        vertical = vertical + Integer.parseInt(parts[1]);
-                        break;
-                    case "up":
-                        vertical = vertical - Integer.parseInt(parts[1]);
-                        break;
-                    default:
-                        break;
-                }
+                instructions.add(new Instruction(parts[0], Integer.parseInt(parts[1])));
             }
         } catch (IOException e) {
-            System.out.println("Something went horribly wrong");
+            System.out.println("Something went horribly wrong " + e);
         }
-        result = forward * vertical;
-        return result;
+
+        return instructions;
+    }
+
+    public static int part1() {
+        int forward = 0;
+        int vertical = 0;
+        ArrayList<Instruction> instructions = readInput("data.txt");
+
+        for (Instruction instruction: instructions) {
+            switch (instruction.command) {
+                case "forward":
+                    forward += instruction.value;
+                    break;
+                case "down":
+                    vertical -= instruction.value;
+                    break;
+                case "up":
+                    vertical += instruction.value;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return forward * Math.abs(vertical);
     }
 
     public static int part2() {
-        int result = 0;
         int forward = 0;
         int aim = 0;
         int depth = 0;
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new FileReader("2021/data.txt"));
-            String l;
-            while ((l = in.readLine()) != null) {
-                String[] parts = l.split(" ");
-                switch (parts[0]) {
-                    case "forward":
-                        forward = forward + Integer.parseInt(parts[1]);
-                        depth = depth + aim * Integer.parseInt(parts[1]);
-                        break;
-                    case "down":
-                        aim = aim + Integer.parseInt(parts[1]);
-                        break;
-                    case "up":
-                        aim = aim - Integer.parseInt(parts[1]);
-                        break;
-                    default:
-                        break;
-                }
+        ArrayList<Instruction> instructions = readInput("data.txt");
+
+        for (Instruction instruction: instructions) {
+            switch (instruction.command) {
+                case "forward":
+                    forward += instruction.value;
+                    depth += aim * instruction.value;
+                    break;
+                case "down":
+                    aim -= instruction.value;
+                    break;
+                case "up":
+                    aim += instruction.value;
+                    break;
+                default:
+                    break;
             }
-        } catch (IOException e) {
-            System.out.println("Something went horribly wrong");
         }
-        result = forward * depth;
-        return result;
+
+        return forward * Math.abs(depth);
     }
 
     public static void main(String[] args) {
