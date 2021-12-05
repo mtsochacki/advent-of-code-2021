4fbbaf4eb57a20e91f4c4de0eba686b66e7304bc
Mateusz Sochacki <mtsochacki@gmail.com>
2021-12-06 00:35:09 +0100

    Rewrite part2() to use new input method and new logic to solve the puzzle

--- 2021/day01/day01.java
+++ 2021/day01/day01.java
@@ -1,15 +1,10 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class day01 {
 
+    // Takes report file and converts it to ArrayList
     public static ArrayList<Integer> readInput(String filename) {
         Scanner sc = null;
         ArrayList<Integer> input = new ArrayList<>();
@@ -23,12 +18,13 @@ public class day01 {
         }
         return input;
     }
+
     // Counts the number of times a depth measurement
     // increases from the previous measurement
     public static void part1() {
         ArrayList<Integer> report = readInput("data.txt");
-        int currentDepth = 0;
         int counter = 0;
+        int currentDepth = 0;
         int previousDepth = report.get(0);
 
         for (int i = 1; i < report.size(); i++) {
@@ -40,45 +36,26 @@ public class day01 {
 
         System.out.println(counter);
     }
-
-    public static int part2() {
-
+    // Counts the number of times a depth measurement
+    // increases from the previous measurement in a 
+    // three-measurement sliding window
+    public static void part2() {
+        ArrayList<Integer> report = readInput("data.txt");
         int counter = 0;
-        int total = 0;
-        int previous = 0;
-        int x = 0;
-        Queue<Integer> q = new LinkedList<Integer>();
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new FileReader("data.txt"));
-            String l;
-            x = Integer.parseInt(in.readLine());
-            q.add(x);
-            total = total + x;
-            x = Integer.parseInt(in.readLine());
-            q.add(x);
-            total = total + x;
-            x = Integer.parseInt(in.readLine());
-            q.add(x);
-            total = total + x;
-            previous = total;
-            while ((l = in.readLine()) != null) {
-                x = Integer.parseInt(l);
-                q.add(x);
-                total = total + x - q.remove();
-                if (total > previous) {
-                    counter++;
-                }
-                previous = total;
-            }
-        } catch (IOException e) {
-            System.out.println("Something went horribly wrong");
-        }
-        return counter;
-    }
+        int currentDepth = 0;
+        int previousDepth = report.get(0);
 
+        for (int i = 0; i < report.size() - 3; i++) {
+            currentDepth = report.get(i + 3);
+            if (currentDepth > previousDepth)
+                counter++;
+            previousDepth = report.get(i + 1);
+        }
+
+        System.out.println(counter);
+    }
     public static void main(String[] args) {
         part1();
-        System.out.println(part2());
+        part2();
     }
 }
