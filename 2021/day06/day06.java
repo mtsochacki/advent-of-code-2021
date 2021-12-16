e11f8b4e2630b3140ecda65dd3ae36d5ae7669db
Mateusz Sochacki <mtsochacki@gmail.com>
2021-12-16 20:25:44 +0100

    Prettify day06

--- 2021/day06/day06.java
+++ 2021/day06/day06.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class day06 {
@@ -18,25 +19,30 @@ public class day06 {
         }
         return input;
     }
-    
+
     public static long calculatePopulation(int days) {
         ArrayList<Integer> listOfFish = readInput("data.txt");
-        ArrayList<Long> popNumbers = new ArrayList<>();
-        for (int i = 0; i < 9; i++) {
-            popNumbers.add(0L);
-        }
-        long totalPopulation = 0;
-        for (Integer fish : listOfFish)
+        ArrayList<Long> popNumbers = new ArrayList<Long>(Collections.nCopies(9, 0L));
+
+        for (Integer fish : listOfFish) {
             popNumbers.set(fish, popNumbers.get(fish) + 1);
+        }
+
         for (int i = 0; i < days; i++) {
             long tmp = popNumbers.get(0);
-            for (int j = 0; j < popNumbers.size() - 1; j++)
+            for (int j = 0; j < popNumbers.size() - 1; j++) {
                 popNumbers.set(j, popNumbers.get(j + 1));
+            }
+
             popNumbers.set(6, popNumbers.get(6) + tmp);
             popNumbers.set(8, tmp);
         }
-        for (Long number : popNumbers)
+
+        long totalPopulation = 0;
+        for (Long number : popNumbers) {
             totalPopulation += number;
+        }
+
         return totalPopulation;
     }
 
