40404e1f3e282ab556e65199800fafd8a77032a4
Mateusz Sochacki <mtsochacki@gmail.com>
2021-12-16 11:43:39 +0100

    Rewrite day10

--- 2021/day10/day10.java
+++ 2021/day10/day10.java
@@ -1,10 +1,22 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class day10 {
+    public static class Scores {
+        int totalSyntaxScore;
+        Long middleScore;
+
+        public Scores(int x, Long y) {
+            totalSyntaxScore = x;
+            middleScore = y;
+        }
+    }
+
     public static ArrayList<ArrayList<Character>> readInput(String filename) {
         ArrayList<ArrayList<Character>> input = new ArrayList<>();
         Scanner sc = null;
@@ -26,58 +38,56 @@ public class day10 {
         return input;
     }
 
-    public static void syntaxScoring() {
+    public static HashMap<Character, Integer> initiateScores() {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put(')', 3);
+        map.put(']', 57);
+        map.put('}', 1197);
+        map.put('>', 25137);
+        map.put('(', 1);
+        map.put('[', 2);
+        map.put('{', 3);
+        map.put('<', 4);
+        return map;
+    }
+
+    public static Scores syntaxScoring() {
         ArrayList<ArrayList<Character>> input = readInput("data.txt");
         ArrayList<Long> autocompleteScores = new ArrayList<>();
+        ArrayList<Character> openingChar = new ArrayList<Character>(Arrays.asList('<', '(', '[', '{'));
+        ArrayList<Character> closingChar = new ArrayList<Character>(Arrays.asList('>', ')', ']', '}'));
+        HashMap<Character, Integer> charScore = initiateScores();
         int syntaxScore = 0;
-
         for (ArrayList<Character> line : input) {
-
             LinkedList<Character> stack = new LinkedList<>();
             Boolean isCorrupted = false;
             long autoTmpScore = 0;
-
-            for (int i = 0; i < line.size(); i++) {
-                if (line.get(i) == '(' || line.get(i) == '<' ||
-                    line.get(i) == '[' || line.get(i) == '{')
-                    stack.addFirst(line.get(i));
-
-                else if (stack.peek() == line.get(i) - 2 || stack.peek() == line.get(i) - 1)
+            for (Character syntaxChar : line) {
+                if (openingChar.contains(syntaxChar)) {
+                    stack.addFirst(syntaxChar);
+                } else if (stack.peek() == syntaxChar - 2 || stack.peek() == syntaxChar - 1) {
                     stack.removeFirst();
-
-                else {  // mark line as corrupted and add to syntax score
+                } else {
                     isCorrupted = true;
-                    if (line.get(i) == ')')
-                        syntaxScore += 3;
-                    else if (line.get(i) == ']')
-                        syntaxScore += 57;
-                    else if (line.get(i) == '}')
-                        syntaxScore += 1197;
-                    else if (line.get(i) == '>')
-                        syntaxScore += 25137;
-                    break;
+                    if (closingChar.contains(syntaxChar)) {
+                        syntaxScore += charScore.get(syntaxChar);
+                        break;
+                    }
                 }
-            }  
-            if (isCorrupted == false) {
+            }
+            if (!isCorrupted) {
                 for (Character element : stack) {
-                    if (element == '(')
-                        autoTmpScore = autoTmpScore * 5 + 1;
-                    else if (element == '[')
-                        autoTmpScore = autoTmpScore * 5 + 2;
-                    else if (element == '{')
-                        autoTmpScore = autoTmpScore * 5 + 3;
-                    else if (element == '<')
-                        autoTmpScore = autoTmpScore * 5 + 4;
+                    autoTmpScore = autoTmpScore * 5 + charScore.get(element);
                 }
                 autocompleteScores.add(autoTmpScore);
             }
         }
-        System.out.println(syntaxScore);
         Collections.sort(autocompleteScores);
-        System.out.println(autocompleteScores.get(autocompleteScores.size() / 2));
+        Scores result = new Scores(syntaxScore, autocompleteScores.get(autocompleteScores.size() / 2));
+        return result;
     }
 
     public static void main(String[] args) {
-        syntaxScoring();
+        System.out.println("Part1 = " + syntaxScoring().totalSyntaxScore + " Part2 = " + syntaxScoring().middleScore);
     }
 }
