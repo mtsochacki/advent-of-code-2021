1318e3ee21bdc48c582fff72b90d06cfac693763
Mateusz Sochacki <mtsochacki@gmail.com>
2021-12-21 13:09:39 +0100

    Unify fold methods into universal one

--- 2021/day13/day13.java
+++ 2021/day13/day13.java
@@ -5,62 +5,36 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class day13 {
-    public static ArrayList<ArrayList<Integer>> readInput(String filename) {
-        ArrayList<ArrayList<Integer>> input = new ArrayList<>();
+    public static ArrayList<ArrayList<Integer>> readCoordinates(String filename) {
+        ArrayList<ArrayList<Integer>> coordinates = new ArrayList<>();
         Scanner sc = null;
-
         try {
             sc = new Scanner(new File(filename));
             sc.useDelimiter(",|\n");
-            for (int i = 0; i < 916; i++) {
+            while (sc.hasNext()) {
                 ArrayList<Integer> line = new ArrayList<>();
                 for (int j = 0; j < 2; j++) {
                     line.add(sc.nextInt());
                 }
-                input.add(line);
+                coordinates.add(line);
             }
         } catch (Exception e) {
             System.out.println("Something went wrong " + e);
         } finally {
             sc.close();
         }
-        return input;
+        return coordinates;
     }
 
-    public static void printAll(ArrayList<ArrayList<Integer>> input) {
-        for (int i = 0; i < input.size(); i++) {
-            for (int j = 0; j < input.get(0).size(); j++) {
-                System.out.println(input.get(i).get(j));
-            }
-            System.out.println();
-        }
-    }
-
-    public static ArrayList<ArrayList<Integer>> foldX(ArrayList<ArrayList<Integer>> input, int x) {
+    public static ArrayList<ArrayList<Integer>> foldPaper(ArrayList<ArrayList<Integer>> input, int x, boolean isX) {
         ArrayList<ArrayList<Integer>> output = new ArrayList<>();
+        int z = isX ? 0 : 1;
         for (ArrayList<Integer> line : input) {
-            if (line.get(0) > x) {
-                int distance = line.get(0) - x;
-                line.set(0, x - distance);
+            if (line.get(z) > x) {
+                int distance = line.get(z) - x;
+                line.set(z, x - distance);
                 output.add(line);
-            } else if (line.get(0) < x) {
-                output.add(line);
-            }
-        }
-        Set<ArrayList<Integer>> set = new HashSet<>(output);
-        output.clear();
-        output.addAll(set);
-        return output;
-    }
-
-    public static ArrayList<ArrayList<Integer>> foldY(ArrayList<ArrayList<Integer>> input, int y) {
-        ArrayList<ArrayList<Integer>> output = new ArrayList<>();
-        for (ArrayList<Integer> line : input) {
-            if (line.get(1) > y) {
-                int distance = line.get(1) - y;
-                line.set(1, y - distance);
-                output.add(line);
-            } else if (line.get(1) < y) {
+            } else if (line.get(z) < x) {
                 output.add(line);
             }
         }
@@ -71,19 +45,19 @@ public class day13 {
     }
 
     public static void part1() {
-        ArrayList<ArrayList<Integer>> input = readInput("data.txt");
-        input = foldX(input, 655);
-        input = foldY(input, 447);
-        input = foldX(input, 327);
-        input = foldY(input, 223);
-        input = foldX(input, 163);
-        input = foldY(input, 111);
-        input = foldX(input, 81);
-        input = foldY(input, 55);
-        input = foldX(input, 40);
-        input = foldY(input, 27);
-        input = foldY(input, 13);
-        input = foldY(input, 6);
+        ArrayList<ArrayList<Integer>> input = readCoordinates("data.txt");
+        input = foldPaper(input, 655, true);
+        input = foldPaper(input, 447, false);
+        input = foldPaper(input, 327, true);
+        input = foldPaper(input, 223, false);
+        input = foldPaper(input, 163, true);
+        input = foldPaper(input, 111, false);
+        input = foldPaper(input, 81, true);
+        input = foldPaper(input, 55, false);
+        input = foldPaper(input, 40, true);
+        input = foldPaper(input, 27, false);
+        input = foldPaper(input, 13, false);
+        input = foldPaper(input, 6, false);
         System.out.println("\033[2J"); // clears screen
         for (ArrayList<Integer> line : input) {
             int row = line.get(1) + 1;
