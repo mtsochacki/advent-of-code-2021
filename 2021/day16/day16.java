f6214da38b6a806a96a728019f45f61977016ec4
Lammatian <mateusz.parafinski@gmail.com>
2021-12-30 19:20:38 +0100

    Modify length calculations in day 16
    Now the length is saved in a packet instead of being returned by the
    parse. This seems to be more intuitive.

--- 2021/day16/day16.java
+++ 2021/day16/day16.java
@@ -12,9 +12,11 @@ public class day16 {
     }
 
     public static abstract class Pack {
+        protected final int HEADER_LENGTH = 6;
         long value;
         int versionID;
         int typeID;
+        int length;
         String binContent;
 
         Pack(String binData) {
@@ -27,7 +29,7 @@ public class day16 {
             return Integer.parseInt(binData.substring(3, 6), 2);
         }
 
-        abstract int parsePacket();
+        abstract void parsePacket();
         abstract int getVersionIdSum();
     }
 
@@ -36,7 +38,7 @@ public class day16 {
             super(binData);
         }
 
-        int parsePacket() {
+        void parsePacket() {
             int idx = 0;
             for (; idx < this.binContent.length(); idx += 5) {
                 String binGroupValue = this.binContent.substring(idx + 1, idx + 5);
@@ -49,7 +51,7 @@ public class day16 {
                 }
             }
 
-            return idx + 6;
+            this.length = idx + 6;
         }
 
         int getVersionIdSum() {
@@ -58,6 +60,7 @@ public class day16 {
     }
 
     public static class OperatorPacket extends Pack {
+        private final int OP_HEADER_LENGTH;
         ArrayList<Pack> subpackets;
         int lengthTypeID;
 
@@ -65,20 +68,47 @@ public class day16 {
             super(binData);
             this.subpackets = new ArrayList<>();
             this.lengthTypeID = Integer.parseInt(this.binContent.substring(0, 1), 2);
+            if (this.lengthTypeID == 0) {
+                this.OP_HEADER_LENGTH = 1 + 15;
+            } else {
+                this.OP_HEADER_LENGTH = 1 + 11;
+            }
         }
 
-        int parsePacket() {
+        void parsePacket() {
+            int subpacketsLength;
+            int subpacketsLengthInfo = Integer.parseInt(this.binContent.substring(1, this.OP_HEADER_LENGTH), 2);
             if (this.lengthTypeID == 0) {
-                int lengthOfPackets = Integer.parseInt(this.binContent.substring(1, 16), 2);
-                int packetsLength = this.parsePacketsByLength(lengthOfPackets);
-                this.calculatePacketValue();
-                return packetsLength + 22;
+                subpacketsLength = this.parseSubpacketsByLength(subpacketsLengthInfo);
             } else {
-                int numberOfPackets = Integer.parseInt(this.binContent.substring(1, 12), 2);
-                int packetsLength = this.parsePacketsByNumber(numberOfPackets);
-                this.calculatePacketValue();
-                return packetsLength + 18;
+                subpacketsLength = this.parseSubpacketsByNumber(subpacketsLengthInfo);
             }
+            this.calculatePacketValue();
+            this.length = this.HEADER_LENGTH + this.OP_HEADER_LENGTH + subpacketsLength;
+        }
+
+        int parseSubpacketsByLength(int lengthOfPackets) {
+            int subpacketsLength = 0;
+            while (subpacketsLength < lengthOfPackets) {
+                Pack packet = initialisePacket(this.binContent.substring(16 + subpacketsLength));
+                packet.parsePacket();
+                subpacketsLength += packet.length;
+                subpackets.add(packet);
+            }
+            return subpacketsLength;
+        }
+
+        int parseSubpacketsByNumber(int numberOfPackets) {
+            int num = 0;
+            int subpacketsLength = 0;
+            while (num < numberOfPackets) {
+                Pack packet = initialisePacket(this.binContent.substring(12 + subpacketsLength));
+                packet.parsePacket();
+                subpacketsLength += packet.length;
+                num += 1;
+                subpackets.add(packet);
+            }
+            return subpacketsLength;
         }
 
         void calculatePacketValue() {
@@ -103,39 +133,9 @@ public class day16 {
             }
         }
 
-        int parsePacketsByLength(int lengthOfPackets) {
-            int idx = 0;
-            while (idx < lengthOfPackets) {
-                Pack packet = initialisePacket(this.binContent.substring(16 + idx));
-                int packetLength = packet.parsePacket();
-                idx += packetLength;
-                subpackets.add(packet);
-            }
-            return idx;
-        }
-
-        int parsePacketsByNumber(int numberOfPackets) {
-            int num = 0;
-            int idx = 0;
-            while (num < numberOfPackets) {
-                Pack packet = initialisePacket(this.binContent.substring(idx + 12));
-                int packetLength = packet.parsePacket();
-                idx += packetLength;
-                num += 1;
-                subpackets.add(packet);
-            }
-            return idx;
-        }
-
         int getVersionIdSum() {
             return this.versionID + this.subpackets.stream().mapToInt(Pack::getVersionIdSum).sum();
         }
-    }
-    public static class Packet {
-        Long value;
-        int length;
-        int versionSum;
-        String binData;
     }
 
     public static String readInput(String filename) {
@@ -159,97 +159,6 @@ public class day16 {
     private static String hexToBin(String hex) {
         String bin = Integer.toBinaryString(Integer.parseInt(hex, 16));
         return String.format("%4s", bin).replace(' ', '0');
-    }
-
-    public static Long makeCalculations(int packetType, ArrayList<Long> values) {
-        switch (packetType) {
-            case 0:
-                Long sum = 0L;
-                for (Long num : values) {
-                    sum += num;
-                }
-                return sum;
-            case 1:
-                Long product = 1L;
-                for (Long num : values) {
-                    product *= num;
-                }
-                return product;
-            case 2:
-                return Collections.min(values);
-            case 3:
-                return Collections.max(values);
-            case 5:
-                return values.get(0) > values.get(1) ? 1L : 0L;
-            case 6:
-                return values.get(0) < values.get(1) ? 1L : 0L;
-            case 7:
-                return values.get(0).equals(values.get(1)) ? 1L : 0L;
-            default:
-                System.out.println("Error in switch statement");
-                return -1L;
-        }
-    }
-
-    public static Packet calculateLiteralValue(Packet packet) {
-        int i = 6;
-        String literalBinValue = "";
-        while (packet.binData.charAt(i) == '1') {
-            literalBinValue += packet.binData.substring(i + 1, i + 5);
-            i += 5;
-        }
-        literalBinValue += packet.binData.substring(i + 1, i + 5);
-        i += 5;
-        packet.binData = packet.binData.substring(i);
-        packet.value = Long.parseLong(literalBinValue, 2);
-        packet.length = i;
-        return packet;
-    }
-
-    public static Packet calculateOperatorZero(Packet packet, int packetTypeID) {
-        int subLength = Integer.parseInt(packet.binData.substring(7, 22), 2);
-        int packetLength = 22;
-        packet.binData = packet.binData.substring(22);
-        ArrayList<Long> results = new ArrayList<>();
-        while (subLength > 0) {
-            packet = processPacket(packet);
-            packetLength += packet.length;
-            subLength -= packet.length;
-            results.add(packet.value);
-        }
-        packet.length = packetLength;
-        packet.value = makeCalculations(packetTypeID, results);
-        return packet;
-    }
-
-    public static Packet calculateOperatorOne(Packet packet, int packetTypeID) {
-        int subAmount = Integer.parseInt(packet.binData.substring(7, 18), 2);
-        int packetLength = 18;
-        packet.binData = packet.binData.substring(18);
-        ArrayList<Long> result = new ArrayList<>();
-
-        for (int j = 0; j < subAmount; j++) {
-            packet = processPacket(packet);
-            packetLength += packet.length;
-            result.add(packet.value);
-        }
-        packet.length = packetLength;
-        packet.value = makeCalculations(packetTypeID, result);
-        return packet;
-    }
-
-    public static Packet processPacket(Packet packet) {
-        packet.versionSum += Integer.parseInt(packet.binData.substring(0, 3),2);
-        int packetTypeID = Integer.parseInt(packet.binData.substring(3, 6), 2);
-        if (packetTypeID == 4) {
-            return calculateLiteralValue(packet);
-        } else {
-            if (packet.binData.charAt(6) == '0') {
-                return calculateOperatorZero(packet, packetTypeID);
-            } else {
-                return calculateOperatorOne(packet, packetTypeID);
-            }
-        }
     }
 
     public static void main(String[] args) {
