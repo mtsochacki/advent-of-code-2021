bbb0e95f762f96b4edea0b49fbb6f1a19e28cc38
Mateusz Sochacki <mtsochacki@gmail.com>
2022-01-01 21:52:00 +0100

    Replace adding nines to input with neighbours method

--- 2021/day09/day09.java
+++ 2021/day09/day09.java
@@ -4,65 +4,77 @@ import java.util.Scanner;
 import java.io.File;
 
 public class day09 {
-    /*
-     * Creates array of arrays surrounded by a border of nines, example:
-     * 9999999
-     * 9123459
-     * 9234569
-     * 9123459
-     * 9999999
-     */
+    public static class Point {
+        int x;
+        int y;
+
+        Point(int hor, int vert) {
+            x = hor;
+            y = vert;
+        }
+    }
+
     public static ArrayList<ArrayList<Integer>> readInput(String filename) {
-        ArrayList<ArrayList<Integer>> input = new ArrayList<>();
-        ArrayList<Integer> emptyLine = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> map = new ArrayList<>();
         Scanner sc;
         try {
             sc = new Scanner(new File(filename));
             sc.useDelimiter("");
-            input.add(emptyLine);
             while (sc.hasNextLine()) {
                 ArrayList<Integer> line = new ArrayList<>();
-                line.add(9);
                 while (sc.hasNextInt()) {
                     line.add(sc.nextInt());
                 }
-                line.add(9);
-                input.add(line);
+                map.add(line);
                 if (sc.hasNextLine())
                     sc.nextLine();
-            }
-            input.add(emptyLine);
-            // Add enough nines to the first and last empty lines
-            for (int i = 0; i < input.get(1).size(); i++) {
-                input.get(0).add(9);
-                input.get(input.size() - 1).add(9);
             }
             sc.close();
         } catch (Exception e) {
             System.out.println("Something went wrong" + e);
         }
-        return input;
+        return map;
     }
 
-    public static boolean isLowPoint(ArrayList<ArrayList<Integer>> input, int x, int y) {
-        return input.get(x).get(y) < input.get(x).get(y - 1) &&
-                input.get(x).get(y) < input.get(x).get(y + 1) &&
-                input.get(x).get(y) < input.get(x - 1).get(y) &&
-                input.get(x).get(y) < input.get(x + 1).get(y);
+    public static ArrayList<Point> getNeighbours(int x, int y, int width, int height) {
+        ArrayList<Point> neighbours = new ArrayList<>();
+        if (x > 0) {
+            neighbours.add(new Point(x - 1, y));
+        }
+        if (x < width - 1) {
+            neighbours.add(new Point(x + 1, y));
+        }
+        if (y > 0) {
+            neighbours.add(new Point(x, y - 1));
+        }
+        if (y < height - 1) {
+            neighbours.add(new Point(x, y + 1));
+        }
+        return neighbours;
+    }
+
+    public static boolean isLowPoint(ArrayList<ArrayList<Integer>> map, int x, int y) {
+        ArrayList<Point> neighbours = getNeighbours(x, y, map.get(y).size(), map.size());
+        for (Point point : neighbours) {
+            if (map.get(x).get(y) >= map.get(point.x).get(point.y)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static int part1() {
-        ArrayList<ArrayList<Integer>> input = readInput("data.txt");
+        ArrayList<ArrayList<Integer>> map = readInput("data.txt");
         int riskLevel = 0;
-        for (int i = 1; i < input.size() - 1; i++) {
-            for (int j = 1; j < input.get(i).size() - 1; j++) {
-                if (isLowPoint(input, i, j))
-                    riskLevel += (input.get(i).get(j) + 1);
+        for (int i = 0; i < map.size(); i++) {
+            for (int j = 0; j < map.get(i).size(); j++) {
+                if (isLowPoint(map, i, j))
+                    riskLevel += (map.get(i).get(j) + 1);
             }
         }
         return riskLevel;
     }
-    // 9 is a border of basin, -1 means we already visited this tile
+
     public static int calculateBasin(ArrayList<ArrayList<Integer>> map, int y, int x) {
         int size = 0;
         if (map.get(y).get(x) == 9 || map.get(y).get(x) == -1) {
@@ -71,20 +83,19 @@ public class day09 {
             size++;
             map.get(y).set(x, -1);
         }
-        size += calculateBasin(map, y, x - 1); // north
-        size += calculateBasin(map, y + 1, x); // east
-        size += calculateBasin(map, y, x + 1); // south
-        size += calculateBasin(map, y - 1, x); // west
+        for (Point point : getNeighbours(x, y, map.get(y).size(), map.size())) {
+            size += calculateBasin(map, point.y, point.x);
+        }
         return size;
     }
 
     public static int part2() {
-        ArrayList<ArrayList<Integer>> input = readInput("data.txt");
+        ArrayList<ArrayList<Integer>> map = readInput("data.txt");
         ArrayList<Integer> result = new ArrayList<>();
-        for (int y = 1; y < input.size() - 1; y++) {
-            for (int x = 1; x < input.get(1).size() - 1; x++) {
-                if (input.get(y).get(x) != -1 && input.get(y).get(x) != 9)
-                    result.add(calculateBasin(input, y, x));
+        for (int y = 0; y < map.size(); y++) {
+            for (int x = 0; x < map.get(1).size(); x++) {
+                if (map.get(y).get(x) != -1 && map.get(y).get(x) != 9)
+                    result.add(calculateBasin(map, y, x));
             }
         }
         Collections.sort(result);
