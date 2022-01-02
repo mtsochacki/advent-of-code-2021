d3a40e12da936fe205ef52c8f2b21a31d9bdf7f7
Mateusz Sochacki <mtsochacki@gmail.com>
2022-01-02 18:42:36 +0100

    Introduce few improvements

--- 2021/day10/day10.java
+++ 2021/day10/day10.java
@@ -8,22 +8,31 @@ import java.util.Scanner;
 public class day10 {
     public static class Line {
         long score;
-        Boolean isCorrupted = false;
+        boolean isCorrupted;
+
+        private String lineStr;
 
         Line(String line) {
+            this.lineStr = line;
+            this.parseLine();
+        }
+
+        void parseLine() {
             LinkedList<Character> stack = new LinkedList<>();
-            for (char c : line.toCharArray()) {
+            for (char c : lineStr.toCharArray()) {
                 if (openingScores.containsKey(c)) {
                     stack.addFirst(c);
-                    /*
-                     * ASCII codes for opening and closing pairs differ
-                     * either by 1 ('(' and ')') or 2 (the others)
-                     */
-                } else if (stack.peek() == c - 2 || stack.peek() == c - 1) {
+                }
+                /*
+                 * ASCII codes for opening and closing pairs differ
+                 * either by 1 ('(' and ')') or 2 (the others)
+                 */
+                else if (stack.peek() == c - 2 || stack.peek() == c - 1) {
                     stack.removeFirst();
                 } else {
                     isCorrupted = true;
                     score += closingScores.get(c);
+                    break;
                 }
             }
             if (!isCorrupted) {
@@ -34,19 +43,19 @@ public class day10 {
         }
     }
 
-    public static ArrayList<Line> readChunks(String filename) {
-        ArrayList<Line> listOfChunks = new ArrayList<>();
+    public static ArrayList<String> readLines(String filename) {
+        ArrayList<String> lines = new ArrayList<>();
         Scanner sc;
         try {
             sc = new Scanner(new File(filename));
             while (sc.hasNextLine()) {
-                listOfChunks.add(new Line(sc.nextLine()));
+                lines.add(sc.nextLine());
             }
             sc.close();
         } catch (Exception e) {
             System.out.println("Something went wrong " + e);
         }
-        return listOfChunks;
+        return lines;
     }
 
     private static final HashMap<Character, Integer> openingScores = new HashMap<>();
@@ -67,16 +76,17 @@ public class day10 {
 
     public static void main(String[] args) {
         long score = 0;
-        ArrayList<Long> allScores = new ArrayList<>();
-        for (Line line : readChunks("data.txt")) {
-            if (line.isCorrupted) {
-                score += line.score;
+        ArrayList<Long> incompleteLineScores = new ArrayList<>();
+        for (String line : readLines("data.txt")) {
+            Line chunk = new Line(line);
+            if (chunk.isCorrupted) {
+                score += chunk.score;
             } else {
-                allScores.add(line.score);
+                incompleteLineScores.add(chunk.score);
             }
         }
-        Collections.sort(allScores);
+        Collections.sort(incompleteLineScores);
         System.out.println(score);
-        System.out.println(allScores.get(allScores.size() / 2));
+        System.out.println(incompleteLineScores.get(incompleteLineScores.size() / 2));
     }
 }
