166195094a1f0407a4082c083d6bcf34ffbf900b
Mateusz Sochacki <mtsochacki@gmail.com>
2022-01-06 23:16:47 +0100

    Introduce getNeighbours, rename variables, cleanup

--- 2021/day15/day15.java
+++ 2021/day15/day15.java
@@ -6,15 +6,22 @@ import java.util.Scanner;
 import java.lang.Integer;
 
 public class day15 {
-    public static class Position implements Comparable<Position> {
+    public static class Point {
         int x;
         int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static class Position extends Point implements Comparable<Position> {
         int risk;
 
-        Position(int vertical, int horizontal, int currentRisk) {
-            x = vertical;
-            y = horizontal;
-            risk = currentRisk;
+        Position(int x, int y, int risk) {
+            super(x, y);
+            this.risk = risk;
         }
 
         @Override
@@ -24,30 +31,30 @@ public class day15 {
     }
 
     public static ArrayList<ArrayList<Integer>> readInput(String filename) {
-        ArrayList<ArrayList<Integer>> input = new ArrayList<>();
-        Scanner sc = null;
+        ArrayList<ArrayList<Integer>> mapOfRisk = new ArrayList<>();
+        Scanner sc;
         try {
             sc = new Scanner(new File(filename));
             while (sc.hasNextLine()) {
-                ArrayList<Integer> line = new ArrayList<>();
+                ArrayList<Integer> riskRow = new ArrayList<>();
                 sc.useDelimiter("");
                 while (sc.hasNextInt()) {
-                    line.add(sc.nextInt());
+                    riskRow.add(sc.nextInt());
                 }
-                input.add(line);
-                if (sc.hasNextLine())
+                mapOfRisk.add(riskRow);
+                if (sc.hasNextLine()) {
                     sc.nextLine();
+                }
             }
+            sc.close();
         } catch (Exception e) {
             System.out.println("Error handling input " + e);
-        } finally {
-            sc.close();
         }
-        return input;
+        return mapOfRisk;
     }
 
-    public static ArrayList<ArrayList<Integer>> modifyInput
-                (ArrayList<ArrayList<Integer>> mapOfRisks, int sizeMultiplier) {
+    public static ArrayList<ArrayList<Integer>> extendMap(
+            ArrayList<ArrayList<Integer>> mapOfRisks, int sizeMultiplier) {
         for (ArrayList<Integer> existingLine : mapOfRisks) {
             int startingLineSize = existingLine.size();
             for (int i = 1; i < sizeMultiplier; i++) {
@@ -75,63 +82,46 @@ public class day15 {
         return mapOfRisks;
     }
 
-    /*
-     * Implementation of Dijkstra's algorithm for finding shortest path between
-     * two nodes. It goes like this:
-     * Do the following as long as we did not reach destination:
-     * 1. Visit all neighbour nodes (that are not in state "already visited")
-     * of the current node. Check their distance (risk) from the starting
-     * position and add them to priority queue. Set their position to
-     * "already visited".
-     * 2. From the priority queue take node that is the closest to starting
-     * position and move there.
-     * 3. Update current position and distance (risk).
-     */
+    public static ArrayList<Point> getNeighbours(int x, int y, int width, int height) {
+        ArrayList<Point> neighbours = new ArrayList<>();
+        if (x > 0) {
+            neighbours.add(new Point(x - 1, y));
+        }
+        if (x < width - 1) {
+            neighbours.add(new Point(x + 1, y));
+        }
+        if (y > 0) {
+            neighbours.add(new Point(x, y - 1));
+        }
+        if (y < height - 1) {
+            neighbours.add(new Point(x, y + 1));
+        }
+        return neighbours;
+    }
+
     public static int calculateRisk(boolean isPart2) {
         ArrayList<ArrayList<Integer>> mapOfRisk = readInput("data.txt");
         if (isPart2) {
-            mapOfRisk = modifyInput(mapOfRisk, 5);
+            mapOfRisk = extendMap(mapOfRisk, 5);
         }
         ArrayList<ArrayList<Boolean>> isVisited = new ArrayList<>();
         for (ArrayList<Integer> element : mapOfRisk) {
-            ArrayList<Boolean> line = new ArrayList<>
-                                (Collections.nCopies(mapOfRisk.size(), false));
+            ArrayList<Boolean> line = new ArrayList<>(Collections.nCopies(mapOfRisk.size(), false));
             isVisited.add(line);
         }
-        PriorityQueue<Position> queOfPositions = new PriorityQueue<>();
+        PriorityQueue<Position> positionQueue = new PriorityQueue<>();
         int x = 0;
         int y = 0;
         int risk = 0;
-        while (x != mapOfRisk.size() - 1 || y != mapOfRisk.size() - 1) {
-            // visit right neighbour
-            if (x < mapOfRisk.size() - 1 && !isVisited.get(y).get(x + 1)) {
-                Position position = new Position(x + 1, y,
-                        risk + mapOfRisk.get(y).get(x + 1));
-                queOfPositions.add(position);
-                isVisited.get(y).set(x + 1, true);
+        while (x != mapOfRisk.get(0).size() - 1 || y != mapOfRisk.size() - 1) {
+            for (Point point : getNeighbours(x, y, mapOfRisk.get(0).size(), mapOfRisk.size())) {
+                Position position = new Position(point.x, point.y, risk + mapOfRisk.get(point.y).get(point.x));
+                if (!isVisited.get(point.y).get(point.x)) {
+                    positionQueue.add(position);
+                }
+                isVisited.get(point.y).set(point.x, true);
             }
-            // visit left neighbour
-            if (x > 0 && !isVisited.get(y).get(x - 1)) {
-                Position position = new Position(x - 1, y,
-                        risk + mapOfRisk.get(y).get(x - 1));
-                queOfPositions.add(position);
-                isVisited.get(y).set(x - 1, true);
-            }
-            // visit top neighbour
-            if (y > 0 && !isVisited.get(y - 1).get(x)) {
-                Position position = new Position(x, y - 1,
-                        risk + mapOfRisk.get(y - 1).get(x));
-                queOfPositions.add(position);
-                isVisited.get(y - 1).set(x, true);
-            }
-            // visit bottom neigbour
-            if (y < mapOfRisk.size() - 1 && !isVisited.get(y + 1).get(x)) {
-                Position position = new Position(x, y + 1,
-                        risk + mapOfRisk.get(y + 1).get(x));
-                queOfPositions.add(position);
-                isVisited.get(y + 1).set(x, true);
-            }
-            Position currentPosition = queOfPositions.poll();
+            Position currentPosition = positionQueue.poll();
             x = currentPosition.x;
             y = currentPosition.y;
             risk = currentPosition.risk;
