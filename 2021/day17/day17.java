c4aa9603984f5ef287792d72b538e41fe70c07ec
Mateusz Sochacki <mtsochacki@gmail.com>
2022-01-14 00:19:53 +0100

    Replace hardcoded input with proper parsing

--- 2021/day17/day17.java
+++ 2021/day17/day17.java
@@ -1,82 +1,102 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
 
 public class day17 {
-    public static boolean isInAreaVertical(int velocity) {
+    public static boolean isYInTrench(int yVelocity, int yStart, int yEnd) {
         int y = 0;
         while (true) {
-            y += velocity;
-            if (y >= -171 && y <= -136) {
+            y += yVelocity;
+            if (y >= yEnd && y <= yStart) {
                 return true;
-            } else if (y < -171) {
+            } else if (y < yEnd) {
                 return false;
             }
-            velocity--;
+            yVelocity--;
         }
     }
 
-    public static int calculateHeight(int velocity) {
+    public static int calculateHeight(int yVelocity) {
         int y = 0;
-        while (velocity > 0) {
-            y += velocity;
-            velocity--;
+        while (yVelocity > 0) {
+            y += yVelocity;
+            yVelocity--;
         }
         return y;
     }
 
-    public static boolean isInAreaHorizontal(int velocity) {
+    public static boolean isXInTrench(int xVelocity, int xStart, int xEnd) {
         int x = 0;
         while (true) {
-            x += velocity;
-            if (x >= 60 && x <= 94) {
+            x += xVelocity;
+            if (x >= xStart && x <= xEnd) {
                 return true;
-            } else if (x > 94 || velocity == 0) {
+            } else if (x > xEnd || xVelocity == 0) {
                 return false;
             }
-            velocity--;
+            xVelocity--;
         }
     }
 
-    public static boolean isInBox(int horizontalVelocity, int verticalVelocity) {
+    public static boolean isInTrench(int xVelocity, int yVelocity,
+            int xStart, int xEnd, int yStart, int yEnd) {
         int x = 0;
         int y = 0;
         while (true) {
-            x += horizontalVelocity;
-            y += verticalVelocity;
-            if (x >= 60 && x <= 94 && y >= -171 && y <= -136) {
+            x += xVelocity;
+            y += yVelocity;
+            if (x >= xStart && x <= xEnd && y >= yEnd && y <= yStart) {
                 return true;
-            } else if (x < 60 && horizontalVelocity == 0) {
+            } else if (x < xStart && xVelocity == 0) {
                 return false;
-            } else if (x > 94 || y < -171) {
+            } else if (x > xEnd || y < yEnd) {
                 return false;
             }
-            if (horizontalVelocity > 0) {
-                horizontalVelocity--;
+            if (xVelocity > 0) {
+                xVelocity--;
             }
-            verticalVelocity--;
+            yVelocity--;
         }
     }
 
     public static void main(String[] args) {
-        int total = 0;
-        ArrayList<Integer> horizontalVelocities = new ArrayList<>();
-        ArrayList<Integer> verticalVelocities = new ArrayList<>();
+        int xStart = 0;
+        int xEnd = 0;
+        int yStart = 0;
+        int yEnd = 0;
+        try {
+            Scanner sc = new Scanner(new File("data.txt"));
+            String[] newLine = sc.nextLine().split(":")[1].split(",");
+            String[] coordinatesX = newLine[0].split("=")[1].split("\\..");
+            String[] coordinatesY = newLine[1].split("=")[1].split("\\..");
+            xStart = Integer.parseInt(coordinatesX[0]);
+            xEnd = Integer.parseInt(coordinatesX[1]);
+            yStart = Integer.parseInt(coordinatesY[1]);
+            yEnd = Integer.parseInt(coordinatesY[0]);
+            sc.close();
+        } catch (Exception e) {
+            System.out.println("There was an error " + e);
+        }
 
-        for (int i = -1000; i < 1000; i++) {
-            if (isInAreaHorizontal(i)) {
-                horizontalVelocities.add(i);
+        ArrayList<Integer> xVelocities = new ArrayList<>();
+        ArrayList<Integer> yVelocities = new ArrayList<>();
+        for (int i = -500; i < 500; i++) {
+            if (isXInTrench(i, xStart, xEnd)) {
+                xVelocities.add(i);
             }
-            if (isInAreaVertical(i)) {
-                verticalVelocities.add(i);
+            if (isYInTrench(i, yStart, yEnd)) {
+                yVelocities.add(i);
             }
         }
-        for (Integer x : horizontalVelocities) {
-            for (Integer y : verticalVelocities) {
-                if (isInBox(x, y)) {
+        int total = 0;
+        for (Integer x : xVelocities) {
+            for (Integer y : yVelocities) {
+                if (isInTrench(x, y, xStart, xEnd, yStart, yEnd)) {
                     total++;
                 }
             }
         }
-        System.out.println(calculateHeight(verticalVelocities.get(verticalVelocities.size() - 1)));
+        System.out.println(calculateHeight(yVelocities.get(yVelocities.size() - 1)));
         System.out.println(total);
     }
 }
