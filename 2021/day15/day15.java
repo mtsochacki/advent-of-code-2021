f921c64fc7d0a2ff75564b92a9f4888dd3033480
Mateusz Sochacki <mtsochacki@gmail.com>
2022-01-19 20:10:00 +0100

    Rewrite logic that extends map in part2

--- 2021/day15/day15.java
+++ 2021/day15/day15.java
@@ -1,9 +1,9 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
-import java.lang.Integer;
 
 public class day15 {
     public static class Point {
@@ -31,55 +31,32 @@ public class day15 {
     }
 
     public static ArrayList<ArrayList<Integer>> readInput(String filename) {
-        ArrayList<ArrayList<Integer>> mapOfRisk = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> mapOfRisks = new ArrayList<>();
         Scanner sc;
         try {
             sc = new Scanner(new File(filename));
             while (sc.hasNextLine()) {
-                ArrayList<Integer> riskRow = new ArrayList<>();
-                sc.useDelimiter("");
-                while (sc.hasNextInt()) {
-                    riskRow.add(sc.nextInt());
-                }
-                mapOfRisk.add(riskRow);
-                if (sc.hasNextLine()) {
-                    sc.nextLine();
-                }
+                mapOfRisks.add(splitRow(sc.nextLine()));
             }
             sc.close();
         } catch (Exception e) {
             System.out.println("Error handling input " + e);
         }
-        return mapOfRisk;
+        return mapOfRisks;
     }
 
-    public static ArrayList<ArrayList<Integer>> extendMap(
-            ArrayList<ArrayList<Integer>> mapOfRisks, int sizeMultiplier) {
-        for (ArrayList<Integer> existingLine : mapOfRisks) {
-            int startingLineSize = existingLine.size();
-            for (int i = 1; i < sizeMultiplier; i++) {
-                for (int y = 0; y < startingLineSize; y++) {
-                    int tmpRisk = existingLine.get(y) + i;
-                    if (tmpRisk > 9)
-                        tmpRisk = tmpRisk % 10 + 1;
-                    existingLine.add(tmpRisk);
-                }
-            }
+    public static ArrayList<Integer> splitRow(String row) {
+        ArrayList<Integer> rowOfRisks = new ArrayList<>();
+        for (String s : new ArrayList<String>(Arrays.asList(row.split("")))) {
+            rowOfRisks.add(Integer.parseInt(s));
         }
-        int startingMapSize = mapOfRisks.size();
-        for (int i = 1; i < sizeMultiplier; i++) {
-            for (int y = 0; y < startingMapSize; y++) {
-                ArrayList<Integer> newLine = new ArrayList<>();
-                for (Integer risk : mapOfRisks.get(y)) {
-                    int tmpRisk = risk + i;
-                    if (tmpRisk > 9)
-                        tmpRisk = tmpRisk % 10 + 1;
-                    newLine.add(tmpRisk);
-                }
-                mapOfRisks.add(newLine);
-            }
-        }
-        return mapOfRisks;
+        return rowOfRisks;
+    }
+
+    public static int getRisk(ArrayList<ArrayList<Integer>> mapOfRisks, int x, int y) {
+        int tmpRisk = mapOfRisks.get(y % mapOfRisks.size()).get(x % mapOfRisks.get(0).size())
+                + (x / mapOfRisks.get(0).size()) + (y / mapOfRisks.size());
+        return tmpRisk < 10 ? tmpRisk : ((tmpRisk % 10) + 1);
     }
 
     public static ArrayList<Point> getNeighbours(int x, int y, int width, int height) {
@@ -100,22 +77,25 @@ public class day15 {
     }
 
     public static int calculateRisk(boolean isPart2) {
-        ArrayList<ArrayList<Integer>> mapOfRisk = readInput("data.txt");
+        ArrayList<ArrayList<Integer>> mapOfRisks = readInput("data.txt");
+        PriorityQueue<Position> positionQueue = new PriorityQueue<>();
+        int height = mapOfRisks.size();
+        int width = mapOfRisks.get(0).size();
         if (isPart2) {
-            mapOfRisk = extendMap(mapOfRisk, 5);
+            height *= 5;
+            width *= 5;
         }
         ArrayList<ArrayList<Boolean>> isVisited = new ArrayList<>();
-        for (ArrayList<Integer> element : mapOfRisk) {
-            ArrayList<Boolean> line = new ArrayList<>(Collections.nCopies(mapOfRisk.size(), false));
+        for (int i = 0; i < height; i++) {
+            ArrayList<Boolean> line = new ArrayList<>(Collections.nCopies(width, false));
             isVisited.add(line);
         }
-        PriorityQueue<Position> positionQueue = new PriorityQueue<>();
         int x = 0;
         int y = 0;
         int risk = 0;
-        while (x != mapOfRisk.get(0).size() - 1 || y != mapOfRisk.size() - 1) {
-            for (Point point : getNeighbours(x, y, mapOfRisk.get(0).size(), mapOfRisk.size())) {
-                Position position = new Position(point.x, point.y, risk + mapOfRisk.get(point.y).get(point.x));
+        while (x != width - 1 || y != height - 1) {
+            for (Point point : getNeighbours(x, y, width, height)) {
+                Position position = new Position(point.x, point.y, risk + getRisk(mapOfRisks, point.x, point.y));
                 if (!isVisited.get(point.y).get(point.x)) {
                     positionQueue.add(position);
                 }
