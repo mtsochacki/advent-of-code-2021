9bc76d2d2ec7dd67ac21dc44c3c0d1ae2b9e2048
Mateusz <mateusz.sochacki@icloud.com>
2022-06-05 17:13:12 +0200

    Rename variables, reduce loop complexity

--- 2021/day25/Day25.java
+++ 2021/day25/Day25.java
@@ -3,6 +3,13 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Day25 {
+    static ArrayList<ArrayList<String>> deepCopyArrayList(ArrayList<ArrayList<String>> source){
+        ArrayList<ArrayList<String>> destination = new ArrayList<>();
+        for (ArrayList<String> strings : source) {
+            destination.add((ArrayList<String>) strings.clone());
+        }
+        return destination;
+    }
     static ArrayList<ArrayList<String>> readCucumberMap(String filename) {
         Scanner sc;
         try {
@@ -10,59 +17,50 @@ public class Day25 {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> cucumberLines = new ArrayList<>();
         while (sc.hasNextLine()) {
-            list.add(sc.nextLine());
+            cucumberLines.add(sc.nextLine());
         }
         sc.close();
         ArrayList<ArrayList<String>> cucumberMap = new ArrayList<>();
-        for (String line : list) {
-            ArrayList<String> splitLine2 = new ArrayList<>(Arrays.asList(line.split("")));
-            cucumberMap.add(splitLine2);
+        for (String line : cucumberLines) {
+            ArrayList<String> splitCucumbers = new ArrayList<>(Arrays.asList(line.split("")));
+            cucumberMap.add(splitCucumbers);
         }
         return cucumberMap;
     }
 
     public static void main(String[] args) {
-        ArrayList<ArrayList<String>> input1 = readCucumberMap("/Users/mateusz/Java/advent-of-code/2021/day25/data.txt");
-        int mapLength = input1.size();
-        int lineLength = input1.get(0).size();
+        ArrayList<ArrayList<String>> firstMap = readCucumberMap("/Users/mateusz/Java/advent-of-code/2021/day25/data.txt");
+        int mapLength = firstMap.size();
+        int lineLength = firstMap.get(0).size();
         int steps = 0;
-        while (true) {
-            ArrayList<ArrayList<String>> newInput = new ArrayList<>();
-
-            for (ArrayList<String> strings : input1) {
-                newInput.add((ArrayList<String>) strings.clone());
-            }
-            boolean hasMoved = false;
+        boolean hasMoved = true;
+        while (hasMoved) {
+            ArrayList<ArrayList<String>> secondMap = deepCopyArrayList(firstMap);
+            hasMoved = false;
             steps++;
             for (int r = 0; r < mapLength; r++) {
                 for (int c = 0; c < lineLength; c++) {
-                    if (input1.get(r).get(c).equals(">") && input1.get(r).get((c + 1) % lineLength).equals(".")) {
-                        newInput.get(r).set(c, ".");
-                        newInput.get(r).set((c + 1) % lineLength, ">");
+                    if (firstMap.get(r).get(c).equals(">") && firstMap.get(r).get((c + 1) % lineLength).equals(".")) {
+                        secondMap.get(r).set(c, ".");
+                        secondMap.get(r).set((c + 1) % lineLength, ">");
                         hasMoved = true;
                     }
                 }
             }
-            ArrayList<ArrayList<String>> lastInput = new ArrayList<>();
-            for (ArrayList<String> strings : newInput) {
-                lastInput.add((ArrayList<String>) strings.clone());
-            }
+            ArrayList<ArrayList<String>> thirdMap = deepCopyArrayList(secondMap);
             for (int r = 0; r < mapLength; r++) {
                 for (int c = 0; c < lineLength; c++) {
-                    if (newInput.get(r).get(c).equals("v") && newInput.get((r + 1) % mapLength).get(c).equals(".")) {
-                        lastInput.get((r + 1) % mapLength).set(c, "v");
-                        lastInput.get(r).set(c, ".");
+                    if (secondMap.get(r).get(c).equals("v") && secondMap.get((r + 1) % mapLength).get(c).equals(".")) {
+                        thirdMap.get((r + 1) % mapLength).set(c, "v");
+                        thirdMap.get(r).set(c, ".");
                         hasMoved = true;
                     }
                 }
             }
-            if (!hasMoved) {
-                System.out.println(steps);
-                break;
-            }
-            input1 = lastInput;
+            firstMap = thirdMap;
         }
+        System.out.println(steps);
     }
 }
