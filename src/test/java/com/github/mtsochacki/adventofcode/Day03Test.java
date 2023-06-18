32bc4da8fb7951df17d5da71892d37fe8a96a9cd
Mateusz Sochacki <mateusz.sochacki@icloud.com>
2023-06-18 11:23:03 +0200

    Add tests to day3

--- src/test/java/com/github/mtsochacki/adventofcode/Day03Test.java
+++ src/test/java/com/github/mtsochacki/adventofcode/Day03Test.java
@@ -6,23 +6,21 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class Day01Test {
-
-    private final Day01 underTest = new Day01();
-    private static final List<String> INPUT = List.of("199", "200", "208", "210", "200", "207", "240", "269", "260", "263");
+class Day03Test {
+    private final Day03 underTest = new Day03();
+    private static final List<String> INPUT = List.of("00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010");
 
     @Test
-    void testCalculatingPart1() {
-        String expected = "7";
+    void test1() {
+        String expected = "198";
 
         String result = underTest.part1(INPUT);
 
         assertThat(result).isEqualTo(expected);
     }
-
     @Test
-    void testCalculatingPart2() {
-        String expected = "5";
+    void test2() {
+        String expected = "230";
 
         String result = underTest.part2(INPUT);
 
