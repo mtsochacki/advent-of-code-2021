67073cc1f3296b920c95afa7c261fbb288a5a735
Mateusz Sochacki <mateusz.sochacki@icloud.com>
2023-06-19 21:57:14 +0200

    Add tests to day12

--- src/test/java/com/github/mtsochacki/adventofcode/Day12Test.java
+++ src/test/java/com/github/mtsochacki/adventofcode/Day12Test.java
@@ -6,24 +6,24 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class Day11Test {
-    private final Day11 underTest = new Day11();
+class Day12Test {
+    private final Day12 underTest = new Day12();
     private static final List<String> INPUT = List.of(
-            "5483143223",
-            "2745854711",
-            "5264556173",
-            "6141336146",
-            "6357385478",
-            "4167524645",
-            "2176841721",
-            "6882881134",
-            "4846848554",
-            "5283751526"
+            "dc-end",
+            "HN-start",
+            "start-kj",
+            "dc-start",
+            "dc-HN",
+            "LN-dc",
+            "HN-end",
+            "kj-sa",
+            "kj-HN",
+            "kj-dc"
     );
 
     @Test
     void test1() {
-        String expected = "1656";
+        String expected = "19";
 
         String result = underTest.part1(INPUT);
 
@@ -32,7 +32,7 @@ class Day11Test {
 
     @Test
     void test2() {
-        String expected = "195";
+        String expected = "103";
 
         String result = underTest.part2(INPUT);
 
