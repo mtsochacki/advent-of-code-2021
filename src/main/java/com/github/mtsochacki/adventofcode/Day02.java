51caa00053c13b6ecf962c04fd32b7293a0941ac
Mateusz Sochacki <87368857+mtsochacki@users.noreply.github.com>
2023-07-02 18:15:30 +0200

    Remove unnecessary code (#37)

--- src/main/java/com/github/mtsochacki/adventofcode/Day02.java
+++ src/main/java/com/github/mtsochacki/adventofcode/Day02.java
@@ -2,33 +2,16 @@ package com.github.mtsochacki.adventofcode;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 public class Day02 implements Day {
-    private class Command {
-        private final String direction;
-        private final int value;
-
-        Command(String direction, int value) {
-            this.direction = direction;
-            this.value = value;
-        }
-    }
-
-    private List<Command> readCommands(List<String> input) {
-        List<Command> listOfCommands = new ArrayList<>();
-        for (String line : input) {
-            String[] tokens = line.split(" ");
-            listOfCommands.add(new Command(tokens[0], Integer.parseInt(tokens[1])));
-        }
-        return listOfCommands;
+    private record Command(String direction, int value) {
     }
 
     @Override
     public String part1(List<String> input) {
-        List<Command> commands = readCommands(input);
+        List<Command> commands = transformIntoCommands(input);
         int forward = 0;
         int vertical = 0;
 
@@ -45,7 +28,7 @@ public class Day02 implements Day {
 
     @Override
     public String part2(List<String> input) {
-        List<Command> commands = readCommands(input);
+        List<Command> commands = transformIntoCommands(input);
         int forward = 0;
         int aim = 0;
         int depth = 0;
@@ -62,5 +45,12 @@ public class Day02 implements Day {
             }
         }
         return String.valueOf(forward * depth);
+    }
+
+    private List<Command> transformIntoCommands(List<String> input) {
+        return input.stream().map(line -> {
+            String[] tokens = line.split(" ");
+            return new Command(tokens[0], Integer.parseInt(tokens[1]));
+        }).toList();
     }
 }
